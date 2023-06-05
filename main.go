3eafa0e8e3b319b4bf94c038e988db211e2d9806
LuiSnake3 <jhonhiv@live.com.mx>
2023-06-05 17:44:55 -0600

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/LuiSnake3/goDesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+estado, texto := variables.ConviertoaTexto(2500)
+fmt.Println(estado)
+fmt.Println(texto)
 }
+
