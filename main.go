d1279a94f127adc19aeb010e11d4ba8ec32527eb
LuiSnake3 <jhonhiv@live.com.mx>
2023-06-07 16:10:39 -0600

    Nuevo Commit

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/LuiSnake3/goDesde0/variables"
+"fmt"
+"github.com/LuiSnake3/goDesde0/ejercicios"	
 )
 
 func main() {
-estado, texto := variables.ConviertoaTexto(2500)
-fmt.Println(estado)
-fmt.Println(texto)
+  /*estado, texto := variables.ConviertoaTexto(2500)
+  fmt.Println(estado)
+  fmt.Println(texto)*/
+
+  /*if os := runtime.GOOS;  os == "linux" || os == "OS X." {
+	fmt.Println("Esto no es Windows, es", os)
+
+  } else {
+    fmt.Println("Esto es Windows")
+  }
+
+  switch os := runtime.GOOS; os {
+  case "linux":
+	fmt.Println("Esto es Linux")
+  case "darwin":
+	fmt.Println("Esto es Darwin")  
+  default:
+	fmt.Printf("%s \n", os)	
+  	
+  }*/
+
+  numero, texto := ejercicios.ConvNumerico("asd")
+  fmt.Println(numero)
+  fmt.Println(texto)
 }
 
